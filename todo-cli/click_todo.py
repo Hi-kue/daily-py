b72805db2a5971e7bd99c45d3b305adf308f6c61
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-05-09 14:40:05 -0400

    refactor: click_todo.py

---
+++ todo-cli/click_todo.py
@@ -0,0 +1,22 @@
+# Built-in Imports
+import sys
+import os
+import re
+import json
+import datetime as dt
+
+
+# External Imports
+import click
+from rich import print, box
+from rich.console import Console
+from rich.live import Live
+from rich.table import Table
+from rich.panel import Panel
+from rich.prompt import Prompt
+
+
+# FIXME Logic for Todo CLI Here.
+
+if __name__ == '__main__':
+    pass
